040dc0f814841749dbb6a86c0fc346e413dfe1f4
philsong <songbohr@gmail.com>
2017-09-12 18:17:06 +0800

    update.

--- hydra/observers/pricemonitor.py
+++ hydra/observers/pricemonitor.py
@@ -58,6 +58,18 @@ class PriceMonitor(Observer):
         fp.write(("%d") % time.time() +','+("%.2f") % diff +'\n')
         fp.close()
 
+        self.render_to_html()
+
         return
 
-  
+    def render_to_html(self):
+        import pandas as pd
+        from pyecharts import Line
+
+        df = pd.read_csv('./data/diff.csv')
+        attr = [i[0] for i in df.values]
+        v1 = [i[1] for i in df.values]
+        line = Line("统计套利-现货期货价差监控")
+        line.add("现货期货价差", attr, v1, mark_point=["average"])
+        # line.add("商家B", attr, v2, is_smooth=True, mark_line=["max", "average"])
+        line.render('./data/index.html')
--- plot.py
+++ plot.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from pyecharts import Line
 
-df = pd.read_csv('diff.csv')
+df = pd.read_csv('./data/diff.csv')
 attr = [i[0] for i in df.values]
 v1 = [i[1] for i in df.values]
-line = Line("okcn/okquanter价差监控")
-line.add("价差", attr, v1, mark_point=["average"])
+line = Line("统计套利-现货期货价差监控")
+line.add("现货期货价差", attr, v1, mark_point=["average"])
 # line.add("商家B", attr, v2, is_smooth=True, mark_line=["max", "average"])
 line.render('./data/index.html')
